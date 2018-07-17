167d2380e14721c2a06096c656e223540dca2543
Simonas Karuzas <simonas@not.cat>
2018-07-17 16:18:56 +0300

    WIP With working login

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
 import './globals'
 import { Linking } from 'react-native'
 
-var uportConnect = require('uport-connect/dist/uport-connect')
-const { Connect, SimpleSigner } = uportConnect
+import Connect from 'uport-connect/dist/uport-connect'
+import { Credentials } from 'uport/dist/uport'
+import { transport, message, crypto } from 'uport-core/dist/uport-core'
+import { decodeJWT, verifyJWT } from 'did-jwt'
+
 
 const uuidv1 = require('uuid/v1')
 const qs = require('qs')
 
 const configureUportConnect = (config) => {
 
+  const callbackUrl = `${config.appAddress}://uport/req`
+  
+  const credentials = new Credentials({
+    address: config.appAddress,
+    privateKey: config.privateKey
+  })
+  
+  const onloadResponse = () => {
+    // Todo return live url
+    return callbackUrl
+  }
+
+  const uportConnect = new Connect(config.appName, {
+    network: 'rinkeby',
+    isMobile: true,
+    onloadResponse,
+  })
+  
+  uportConnect.credentials = credentials
+
+  uportConnect.genCallback = () => {
+    return callbackUrl
+  }
+  
   const uriHandler = (url) => {
     Linking.openURL(url)
   }
+  uportConnect.mobileTransport = transport.url.send({uriHandler})
 
-  const uport = new Connect(config.appName, {
-    clientId: config.appAddress,
-    signer: SimpleSigner(config.privateKey),
-    mobileUrlHandler: uriHandler,
-    uriHandler: uriHandler,
-  })
 
-  uport.topicFactory = (name) => {
-    const id = uuidv1()
-    const url = `${config.appAddress}:${id}`
-    let handler
-    let cancel
-    const topic = new Promise((resolve, reject) => {
-      handler = (uri) => {
-        if (uri && uri.url.startsWith(url)) {
-          const decoded = uri.url.replace('%23', '#')
-          const params = qs.parse(decoded.slice(decoded.search(/\#/)+1))
-          if (params && params[name]) {
-            Linking.removeEventListener('url', handler)
-            resolve(params[name])
-          } else {
-            reject()
-          }          
-        }
-      }
-      Linking.addEventListener('url', handler)
-
-      cancel = () => {
-        Linking.removeEventListener('url', handler)
-        resolve()
-      }
-    })
-    topic.url = url
-    topic.cancel = cancel
-    return topic
+  uportConnect.requestDisclosure = (options) => {
+    const reqObj = { ...options,
+                     notifications: false,
+                     callbackUrl: callbackUrl }
+    return uportConnect.credentials.requestDisclosure(reqObj)
   }
 
-  return {
-    ...uportConnect,
-    uport,
+  uportConnect.requestMobile = (JWT, id) => {
+    return uportConnect.request(JWT, id, {type: 'redirect', redirectUrl: callbackUrl})
   }
+
+  const linkingHandler = (uri) => {
+    uportConnect.pubResponse(transport.url.parseResponse(uri.url))
+  }
+
+  Linking.addEventListener('url', linkingHandler)
+
+
+  return uportConnect
 }
 
 export default configureUportConnect
